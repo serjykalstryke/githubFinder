a6a6e716bc26480ab835d876ad37eaa06dfcf67f
David Stinnett <davidstinnett@icloud.com>
2021-01-22 01:35:51 -0500

    got functionality back to where I originally had issues and I am not experiencing those issues. started on user page, just need to flesh out UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
@@ -11,6 +12,7 @@ import "./App.css";
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null,
 	};
@@ -25,6 +27,16 @@ class App extends Component {
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	//Get Single Github User
+	getUser = async (username) => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+		);
+
+		this.setState({ user: res.data, loading: false });
+	};
+
 	//Clear Users From State
 
 	clearUsers = () => this.setState({ users: [], loading: false });
@@ -36,7 +48,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { users, loading } = this.state;
+		const { users, user, loading } = this.state;
 		return (
 			<Router>
 				<div className="app">
@@ -60,6 +72,18 @@ class App extends Component {
 								)}
 							/>
 							<Route exact path="/about" component={About} />
+							<Route
+								exact
+								path="/user/:login"
+								render={(props) => (
+									<User
+										{...props}
+										getUser={this.getUser}
+										user={user}
+										loading={loading}
+									/>
+								)}
+							/>
 						</Switch>
 					</div>
 				</div>
