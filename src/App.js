3041cdd7f18cc87e4a6a79ac5b6fa39d43d144c1
David Stinnett <davidstinnett@icloud.com>
2021-01-22 19:19:46 -0500

    finalized application

--- src/App.js
+++ src/App.js
@@ -1,110 +1,36 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import axios from "axios";
+import NotFound from "./components/pages/NotFound";
+
 import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
 import "./App.css";
 
 const App = () => {
-	const [users, setUsers] = useState([]);
-	const [user, setUser] = useState({});
-	const [repos, setRepos] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [alert, setAlert] = useState(null);
-
-	//Search Github Users
-	const searchUsers = async (text) => {
-		setLoading(true);
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
-		);
-
-		setUsers(res.data.items);
-		setLoading(false);
-	};
-
-	//Get Single Github User
-	const getUser = async (username) => {
-		setLoading(true);
-		const res = await axios.get(
-			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
-		);
-
-		setUser(res.data);
-		setLoading(false);
-	};
-
-	//Get Users Repos
-	const getUserRepos = async (username) => {
-		setLoading(true);
-
-		const res = await axios.get(
-			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
-		);
-		setRepos(res.data);
-		setLoading(false);
-	};
-
-	//Clear Users From State
-
-	const clearUsers = () => {
-		setUsers([]);
-		setLoading(false);
-	};
-
-	//Alert function
-	const showAlert = (msg, type) => {
-		setAlert({ msg, type });
-
-		setTimeout(() => setAlert(null), 3500);
-	};
 	return (
 		<GithubState>
-			<Router>
-				<div className="app">
-					<Navbar />
-					<div className="container">
-						<Alert alert={alert} />
-						<Switch>
-							<Route
-								exact
-								path="/"
-								render={(props) => (
-									<Fragment>
-										<Search
-											searchUsers={searchUsers}
-											clearUsers={clearUsers}
-											showClear={users.length > 0 ? true : false}
-											setAlert={showAlert}
-										/>
-										<Users loading={loading} users={users} />
-									</Fragment>
-								)}
-							/>
-							<Route exact path="/about" component={About} />
-							<Route
-								exact
-								path="/user/:login"
-								render={(props) => (
-									<User
-										{...props}
-										getUser={getUser}
-										getUserRepos={getUserRepos}
-										user={user}
-										repos={repos}
-										loading={loading}
-									/>
-								)}
-							/>
-						</Switch>
+			<AlertState>
+				<Router>
+					<div className="app">
+						<Navbar />
+						<div className="container">
+							<Alert />
+							<Switch>
+								<Route exact path="/" component={Home} />
+								<Route exact path="/about" component={About} />
+								<Route exact path="/user/:login" component={User} />
+								<Route component={NotFound} />
+							</Switch>
+						</div>
 					</div>
-				</div>
-			</Router>
+				</Router>
+			</AlertState>
 		</GithubState>
 	);
 };
